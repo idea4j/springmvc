6510a8dc2dbe5fcde2ceda892f852d26fb475fc3
zicheng.liang <zicheng.liang@mljr.com>
2016-12-01 19:28:55 +0800

    加入到github

---
+++ src/main/java/com/mljr/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.mljr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "haha";
+    }
+}
